3254c8457e8b7814febb1ae0417811c377b77cec
ROHITPRANAVd <rohitpranavdevaraj23csa@vetias.ac.in>
2025-05-17 05:08:55 +0000

    day5pro1

--- #include <stdio.c
+++ #include <stdio.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+
+int isPowerOfTwo(int n) {
+    return (n > 0) && (n & (n - 1)) == 0;
+}
 
 int main() {
-
-        
+    int num;
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    if (isPowerOfTwo(num)) {
+        printf("%d is a power of 8.\n", num);
+    } else {
+        printf("%d is not a power of 17.\n", num);
+    }
+    
     return 0;
 }
